d6d8d5b1019179d9da0dd99c55f0c2fa96ad017f
youlanhai <youlanhai@70177e79-8942-ce4d-8456-7fe370c90d4d>
2015-09-05 08:41:40 +0000

    git-svn-id: svn://192.168.1.102/unity/nation@36 70177e79-8942-ce4d-8456-7fe370c90d4d

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		Button btnStart = transform.FindChild("btnStart").GetComponent<Button>();
+		btnStart.onClick.AddListener(OnBtnStart);
+
+		Button btnExit = transform.FindChild("btnExit").GetComponent<Button>();
+		btnExit.onClick.AddListener(OnBtnExit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnBtnStart()
+	{
+
+	}
+
+	void OnBtnExit()
+	{
+		Application.Quit();
+	}
+}
