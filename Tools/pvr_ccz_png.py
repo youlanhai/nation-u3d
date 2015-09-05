f0d196a41479cc3419a44a2131107b59192f3317
youlanhai <youlanhai@70177e79-8942-ce4d-8456-7fe370c90d4d>
2015-09-05 04:20:06 +0000

    git-svn-id: svn://192.168.1.102/unity/nation@25 70177e79-8942-ce4d-8456-7fe370c90d4d

--- Tools/pvr_ccz_png.py
+++ Tools/pvr_ccz_png.py
@@ -3,24 +3,40 @@
 import os
 import sys
 
+USAGE = """
+usage:
+python xxx.py path extension [remove_old_file]
+"""
 
-def main(path):
-	print "path:", path
+def usage(): print USAGE
+
+def convert(path, src_ext, rm_old=False):
+	print "root path:", path
+
+	src_ext_lenght = len(src_ext)
 
 	for dirpath, dirnames, filenames in os.walk(path):
-		#print "path:", dirpath
 
 		for fname in filenames:
-			if len(fname) > 8 and fname[-8:] == ".pvr.ccz":
-				name = fname[:-8]
+			if len(fname) > src_ext_lenght and fname[-src_ext_lenght:].lower() == src_ext:
+				name = fname[:-src_ext_lenght]
 
-				fullPath = os.path.join(dirpath, name)
+				srcFullPath = os.path.join(dirpath, fname)
+				dstFullPath = os.path.join(dirpath, name + ".png")
 
-				cmd = """TexturePacker.exe --opt RGBA8888 --sheet "%s.png" "%s.pvr.ccz" """ % (fullPath, fullPath)
+				cmd = """TexturePacker.exe --opt RGBA8888  --size-constraints AnySize --padding 0 --sheet "%s" "%s" """ % (dstFullPath, srcFullPath, )
 
-				print "convert: ", fullPath
-				os.system(cmd)
+				print "convert: ", srcFullPath
+				if 0 == os.system(cmd):
+					if rm_old: os.remove(os.path.join(dirpath, fname))
+
+def main():
+	if len(sys.argv) < 3:
+		return usage()
+
+	rm_old = sys.argv[3] == "true" if len(sys.argv) > 3 else False
+
+	convert(sys.argv[1], sys.argv[2], rm_old)
 
 if __name__ == "__main__":
-	if len(sys.argv) >= 2:
-		main(sys.argv[1])
+	main()
