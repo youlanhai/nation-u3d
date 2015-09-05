da461ee0300fc549b99e0aafd22144bfec655a7e
youlanhai <youlanhai@70177e79-8942-ce4d-8456-7fe370c90d4d>
2015-09-05 05:36:37 +0000

    git-svn-id: svn://192.168.1.102/unity/nation@27 70177e79-8942-ce4d-8456-7fe370c90d4d

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+	public float scrollSpeed_ = -0.1f;
+	public float tileSizeZ_ = 7.4f;
+	
+	private Vector3 startPosition_;
+	
+	void Start ()
+	{
+		startPosition_ = transform.position;
+	}
+	
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed_, tileSizeZ_);
+		transform.position = new Vector3(startPosition_.x, startPosition_.y + newPosition, startPosition_.z);
+	}
+}
