3e7d3c22917d6fbdc6201893631a234073965152
youlanhai <youlanhai@70177e79-8942-ce4d-8456-7fe370c90d4d>
2015-09-05 10:15:09 +0000

    git-svn-id: svn://192.168.1.102/unity/nation@38 70177e79-8942-ce4d-8456-7fe370c90d4d

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,57 @@ using System.Collections;
 
 namespace mygame
 {
+	enum FlyDirection
+	{
+		None,
+		Left,
+		Right,
+	};
+
 	public class Player : MonoBehaviour
 	{
+		Animator 		animator_;
+		FlyDirection	direction_ = FlyDirection.None;
 
+		public float 	moveSpeed_ = 1.0f;
+
+		void Start()
+		{
+			animator_ = GetComponent<Animator>();
+		}
+
+		void Update()
+		{
+			float horizontal = Input.GetAxis("Horizontal");
+			float vertical = Input.GetAxis("Vertical");
+
+			Vector3 delta = new Vector3(horizontal, vertical, 0);
+			transform.position += delta * moveSpeed_ * Time.deltaTime;
+
+			if(horizontal > 0)
+			{
+				SetDirection(FlyDirection.Left);
+			}
+			else if(horizontal < 0)
+			{
+				SetDirection(FlyDirection.Right);
+			}
+		}
+
+		void SetDirection(FlyDirection dir)
+		{
+			if(direction_ != dir)
+			{
+				direction_ = dir;
+				if(direction_ == FlyDirection.Left)
+				{
+					animator_.Play("fly_right");
+				}
+				else if(direction_ == FlyDirection.Right)
+				{
+					animator_.Play("fly_left");
+				}
+			}
+		}
 	};
 }
